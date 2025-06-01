5355220d7da39dcc6e20a1583b2872b2ef11b752
tss0-ui <travisscottsmith0@gmail.com>
2025-06-01 04:56:45 -0400

    Create model_architecture.py

---
+++ ml-models/training/model_architecture.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+def build_model(input_shape, num_classes):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Conv2D(16, (3, 3), activation='relu', input_shape=input_shape),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2, 2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(num_classes, activation='softmax')
+    ])
+    model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    return model
