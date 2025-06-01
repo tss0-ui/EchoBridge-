e036d79a381eac31aa166cf702600a8d2161da8b
tss0-ui <travisscottsmith0@gmail.com>
2025-06-01 10:24:06 -0400

    Create App.js

---
+++ mobile/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import HomeScreen from './screens/HomeScreen';
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <HomeScreen />
+    </NavigationContainer>
+  );
+}
